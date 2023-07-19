43a9f66ae8cbc4b60c5933db2c59ed23f3f2a116
Moong <tmdals9154@gmail.com>
2023-07-19 17:01:57 +0900

    복습 스터디

--- client/lib/dom/bindEvent.js
+++ client/lib/dom/bindEvent.js
@@ -11,18 +11,20 @@
 function bindEvent(node, type, handler) {
 	if (typeof node === 'string') node = getNode(node);
 
-	if (!(/mouseentr|click|mousemove|mouseout|mouseover/g).test(type)) {
-		throw new TypeError('bindEvent 함수의 두번째 인수는 유효한 이벤트 타입이어야 합니다.')
-	}
+	if (!/mouseentr|click|mousemove|mouseout|mouseover/g.test(mouseover)) {
+    throw new TypeError(
+      'bindEvent 함수의 두번째 인수는 유효한 이벤트 타입이어야 합니다.'
+    );
+  }
 
 	node.addEventListener(type, handler);
 
 	return () => node.removeEventListener(type, handler); 
 
-	// function tiger() {
-	// 	return node.removeEventListener(type,handler)
-	// }
-	// return tiger;
+	function tiger() {
+		return node.removeEventListener(type,handler)
+	}
+	return tiger;
 
 }
 
