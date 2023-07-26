a11fe6dcb133376ac7c1369720a8b00dd11940c1
Moong <tmdals9154@gmail.com>
2023-07-27 08:56:27 +0900

    fetch, await, async 복습 스터디

--- client/lib/utils/tiger.js
+++ client/lib/utils/tiger.js
@@ -3,13 +3,23 @@
 const URL = 'https://jsonplaceholder.typicode.com/users';
 
 
-// async에 속해있지 않더라도 promise 객체를 뒤에 두고 있다면 await을 사용할수 있다.
 
-/* //# fetch 요청은 2개의 await 호출로 구성되어 있다.
+//% 1. fetch만 했을때 
+	//* Promise {<pending>} / [[PromiseResult]] : Response
+//% 2. await fetch 했을때
+	//* Response{type: 'cors', url: 'https://jsonplaceholder.typicode.com/users', redirected: false, status: 200, ok: true …}
+//% 3. response.json() 했을때
+	//* Promise {<pending>} / [[PromiseResult]] : Array(10)
+//% 4. await response.json() 했을때
+	//* (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+
+
+//@ async에 속해있지 않더라도 promise 객체를 뒤에 두고 있다면 await을 사용할수 있다.
+/* //# fetch 요청은 2개의 await 호출로 구성되어 있다. (2번의 await을 거쳐야 원하는 데이터 값이 도출된다.)
 // response는 현재 상태 이고, data를 보기위해서 한번더 await을 해야한다.
-const response = await fetch('https://pokeapi.co/api/v2/pokemon/30');
+const response = await fetch(URL);
 
-console.log(response);		// 통신에 대한 상태? 값이 담겨 있음.
+console.log(response);		// 통신에 대한 상태? 값이 담겨 있음. (fetch해서 가져온 데이터를 까보면)
 
 if(response.ok) {
 	const data = await response.json();		// response에서 한번더 까주면 원하는 데이터가 나온다.
@@ -28,14 +38,15 @@ const defaultOptions = {
 	}
 }
 
+
 /* -------------------------------------------------------------------------- */
-// async는 프로미스 객체를 반환하기때문에 return에서 굳이 await을 해줄 필요가 없다.
+//% async는 프로미스 객체를 반환하기때문에 return에서 굳이 await을 해줄 필요가 없다.
 export const tiger = async (options) => {
 
 	const {url, ...restOptions} = {
 		...defaultOptions,
 		...options,
-		headres: {
+		headres: {		// 깊은 복사를 하기 위해서 headers를 한번더 복사 해준거다.
 			...defaultOptions.headres,
 			...options.headres
 		}
@@ -90,17 +101,10 @@ tiger.put = (url, body, options) => {
 		...options
 	})
 }
+
 // const data = await tiger.get(URL)
 
 // console.log(await data.data);
-
-
-
-
-
-
-
-
 
 
 
@@ -120,51 +124,8 @@ console.log(userData);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // const data = await tiger(URL);
 
 // console.log(await tiger('data'));
 
 // tiger('https://pokeapi.co/api/v2/pokemon/30')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- client/main.js
+++ client/main.js
@@ -5,6 +5,7 @@ import {tiger} from "./lib/index.js";
  
 
 
-const data = await tiger.get('https://jsonplaceholder.typicode.com/users');
+// const data = await tiger.get('https://jsonplaceholder.typicode.com/users');
 
-console.log(data);
+
+// console.log(data);
